91d194d5ec5372c959305907799dae56938f811c
Justin Leong <justinleong16@gmail.com>
2017-12-28 11:19:25 -0800

    Finish part v builders

--- src/v_builders/n39HtmlBuilders.kt
+++ src/v_builders/n39HtmlBuilders.kt
@@ -22,7 +22,7 @@ fun todoTask39(): Nothing = TODO(
 fun renderProductTable(): String {
     return html {
         table {
-            tr {
+            tr (color = getTitleColor()) {
                 td {
                     text("Product")
                 }
@@ -34,7 +34,21 @@ fun renderProductTable(): String {
                 }
             }
             val products = getProducts()
-            todoTask39()
+            products.mapIndexed {
+                idx, it ->
+                tr {
+                    td (getCellColor(idx, 0)) {
+                        text(it.description)
+                    }
+                    td (getCellColor(idx, 1)){
+                        text(it.price)
+                    }
+                    td (getCellColor(idx, 2)){
+                        text(it.popularity)
+                    }
+                }
+
+            }
         }
     }.toString()
 }
